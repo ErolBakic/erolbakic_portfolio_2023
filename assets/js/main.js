ab39594366c6e70046f1273d2ed0f177337a1c0a
ErolBakic <erol.bakic@hotmail.com>
2023-10-03 23:34:44 +0200

    no btn option

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,45 +83,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
             // Btns
             target.querySelector('.btn').innerHTML = '';
-            content.links.reverse().forEach(link => {
+            if(content.links){
+                content.links.reverse().forEach(link => {
 
-                // Regular btn
-                if(!link['goto']['url'].includes('#')){
-                    const output = (`
-                        <a target="_blank" href="${link['goto']['url']}" class="btn__link">
-                            <span class="btn__gfx">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="23.589" height="23.589" viewBox="0 0 23.589 23.589">
-                                <path id="Path_13" data-name="Path 13" d="M19.9,14.743H18.429a.737.737,0,0,0-.737.737v5.16H2.949V5.9H9.583a.737.737,0,0,0,.737-.737V3.686a.737.737,0,0,0-.737-.737H2.211A2.211,2.211,0,0,0,0,5.16V21.377a2.211,2.211,0,0,0,2.211,2.211H18.429a2.211,2.211,0,0,0,2.211-2.211v-5.9A.737.737,0,0,0,19.9,14.743ZM21.869,0h-7.31a1.29,1.29,0,0,0-1.29,1.29v.862A1.29,1.29,0,0,0,14.6,3.4l3.1-.088L6.222,14.7l0,0a1.106,1.106,0,0,0,0,1.564l1.1,1.1,0,0a1.106,1.106,0,0,0,1.562,0L20.272,5.9l-.087,3.1V9.03a1.29,1.29,0,0,0,1.29,1.29H22.3a1.29,1.29,0,0,0,1.29-1.29V1.72h0A1.72,1.72,0,0,0,21.869,0Z" fill="#282a2e"></path>
-                                </svg>
-                            </span>
-                            ${link['goto']['title']}                    
-                        </a>
-                    `);
-                    target.querySelector('.btn').insertAdjacentHTML('afterbegin', output);
-                    
-                // Special btn
-                } else {
-                    const delimeters = link['goto']['url'].split('#');
-                    const attributes = delimeters[1].split('=');
-
-                    const output = (`
-                        <form target="_blank" action="${delimeters[0]}" method="post">
-                            <button type="submit" class="btn__link btn__link--button">
-                                <input type="hidden" name="${attributes[0]}" value="${attributes[1]}">
-
+                    // Regular btn
+                    if(!link['goto']['url'].includes('#')){
+                        const output = (`
+                            <a target="_blank" href="${link['goto']['url']}" class="btn__link">
                                 <span class="btn__gfx">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="23.589" height="23.589" viewBox="0 0 23.589 23.589">
-                                        <path id="Path_13" data-name="Path 13" d="M19.9,14.743H18.429a.737.737,0,0,0-.737.737v5.16H2.949V5.9H9.583a.737.737,0,0,0,.737-.737V3.686a.737.737,0,0,0-.737-.737H2.211A2.211,2.211,0,0,0,0,5.16V21.377a2.211,2.211,0,0,0,2.211,2.211H18.429a2.211,2.211,0,0,0,2.211-2.211v-5.9A.737.737,0,0,0,19.9,14.743ZM21.869,0h-7.31a1.29,1.29,0,0,0-1.29,1.29v.862A1.29,1.29,0,0,0,14.6,3.4l3.1-.088L6.222,14.7l0,0a1.106,1.106,0,0,0,0,1.564l1.1,1.1,0,0a1.106,1.106,0,0,0,1.562,0L20.272,5.9l-.087,3.1V9.03a1.29,1.29,0,0,0,1.29,1.29H22.3a1.29,1.29,0,0,0,1.29-1.29V1.72h0A1.72,1.72,0,0,0,21.869,0Z" fill="#282a2e"></path>
+                                    <path id="Path_13" data-name="Path 13" d="M19.9,14.743H18.429a.737.737,0,0,0-.737.737v5.16H2.949V5.9H9.583a.737.737,0,0,0,.737-.737V3.686a.737.737,0,0,0-.737-.737H2.211A2.211,2.211,0,0,0,0,5.16V21.377a2.211,2.211,0,0,0,2.211,2.211H18.429a2.211,2.211,0,0,0,2.211-2.211v-5.9A.737.737,0,0,0,19.9,14.743ZM21.869,0h-7.31a1.29,1.29,0,0,0-1.29,1.29v.862A1.29,1.29,0,0,0,14.6,3.4l3.1-.088L6.222,14.7l0,0a1.106,1.106,0,0,0,0,1.564l1.1,1.1,0,0a1.106,1.106,0,0,0,1.562,0L20.272,5.9l-.087,3.1V9.03a1.29,1.29,0,0,0,1.29,1.29H22.3a1.29,1.29,0,0,0,1.29-1.29V1.72h0A1.72,1.72,0,0,0,21.869,0Z" fill="#282a2e"></path>
                                     </svg>
                                 </span>
-                                ${link['goto']['title']}
-                            </button>
-                        </form>
-                    `);
-                    target.querySelector('.btn').insertAdjacentHTML('afterbegin', output);
-                }
+                                ${link['goto']['title']}                    
+                            </a>
+                        `);
+                        target.querySelector('.btn').insertAdjacentHTML('afterbegin', output);
+                        
+                    // Special btn
+                    } else {
+                        const delimeters = link['goto']['url'].split('#');
+                        const attributes = delimeters[1].split('=');
 
-            });
+                        const output = (`
+                            <form target="_blank" action="${delimeters[0]}" method="post">
+                                <button type="submit" class="btn__link btn__link--button">
+                                    <input type="hidden" name="${attributes[0]}" value="${attributes[1]}">
+
+                                    <span class="btn__gfx">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="23.589" height="23.589" viewBox="0 0 23.589 23.589">
+                                            <path id="Path_13" data-name="Path 13" d="M19.9,14.743H18.429a.737.737,0,0,0-.737.737v5.16H2.949V5.9H9.583a.737.737,0,0,0,.737-.737V3.686a.737.737,0,0,0-.737-.737H2.211A2.211,2.211,0,0,0,0,5.16V21.377a2.211,2.211,0,0,0,2.211,2.211H18.429a2.211,2.211,0,0,0,2.211-2.211v-5.9A.737.737,0,0,0,19.9,14.743ZM21.869,0h-7.31a1.29,1.29,0,0,0-1.29,1.29v.862A1.29,1.29,0,0,0,14.6,3.4l3.1-.088L6.222,14.7l0,0a1.106,1.106,0,0,0,0,1.564l1.1,1.1,0,0a1.106,1.106,0,0,0,1.562,0L20.272,5.9l-.087,3.1V9.03a1.29,1.29,0,0,0,1.29,1.29H22.3a1.29,1.29,0,0,0,1.29-1.29V1.72h0A1.72,1.72,0,0,0,21.869,0Z" fill="#282a2e"></path>
+                                        </svg>
+                                    </span>
+                                    ${link['goto']['title']}
+                                </button>
+                            </form>
+                        `);
+                        target.querySelector('.btn').insertAdjacentHTML('afterbegin', output);
+                    }
+
+                });
+            }
 
             // Cover
             if(target.querySelector('.video')){
